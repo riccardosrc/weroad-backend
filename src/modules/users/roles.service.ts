c494e931dd5109dd7b8def95f8ea1fd762d733f1
riccardofonte <riccardo.fonte@kotuko.it>
2024-01-18 16:35:00 +0100

    feat(roles): user roles service

---
+++ src/modules/users/roles.service.ts
@@ -0,0 +1,58 @@
+import {
+  Injectable,
+  Logger,
+  NotFoundException,
+  OnApplicationBootstrap,
+} from '@nestjs/common';
+import { EnsureRequestContext, MikroORM } from '@mikro-orm/core';
+import { Role } from './entities/role.entity';
+import { EntityManager } from '@mikro-orm/postgresql';
+
+@Injectable()
+export class RolesService implements OnApplicationBootstrap {
+  private logger: Logger;
+
+  constructor(
+    private em: EntityManager,
+    private readonly orm: MikroORM,
+  ) {
+    this.logger = new Logger(RolesService.name);
+  }
+
+  async create(name: string) {
+    const newRole = this.em.create(Role, { name });
+    await this.em.persistAndFlush(newRole);
+    return newRole;
+  }
+
+  async findAll() {
+    const roles = await this.em.findAll(Role);
+    return roles;
+  }
+
+  async findOne(id: string) {
+    const role = await this.em.findOne(Role, { id: id });
+    if (!role) {
+      throw new NotFoundException(`role with id ${id} not found`);
+    }
+    return role;
+  }
+
+  async findByIds(ids: string[]) {
+    const roles = await this.em.findAll(Role, {
+      where: { id: { $in: ids } },
+    });
+    return roles;
+  }
+
+  @EnsureRequestContext()
+  async onApplicationBootstrap() {
+    const roles = await this.findAll();
+    if (roles.length > 0) {
+      return;
+    }
+    await this.create('admin');
+    await this.create('user');
+    this.logger.log('roles seed completed');
+  }
+}
--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -4,9 +4,10 @@ import { UsersResolver } from './users.resolver';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Role } from './entities/role.entity';
 import { User } from './entities/user.entity';
+import { RolesService } from './roles.service';
 
 @Module({
   imports: [MikroOrmModule.forFeature([Role, User])],
-  providers: [UsersResolver, UsersService],
+  providers: [UsersResolver, UsersService, RolesService],
 })
 export class UsersModule {}
